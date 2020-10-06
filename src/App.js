d53d8d44657b5b7d049f906b17c24be86090601c
Dennis Han <dennis@Denniss-MacBook-Pro.local>
2020-10-05 23:54:53 -0700

    convert proj to jsx, add react-specific ESLint config, useState, custom hooks,

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, { name: "Meepo", animal: "Cat", breed: "mixed" }),
-    React.createElement(Pet, { name: "max", animla: "Dog", breed: "golden" }),
-  ]);
+  return (
+    <div id="something-important">
+      <h1>Adopt me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
